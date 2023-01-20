36bf3cefa5a4b0bc4dacc1b1c776854192cec575
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-20 09:36:48 +0200

    part one
    part one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let intervalId = null;
+const bodyDocument = document.querySelector('body');
+
+const StartButton = document.querySelector('[data-start]');
+StartButton.addEventListener('click', onStartButtonClick);
+
+const StopButton = document.querySelector('[data-stop]');
+StopButton.addEventListener('click', onStopButtonClick);
+
+StopButton.disabled = true;
+
+function onStartButtonClick() {
+  StartButton.disabled = true;
+  StopButton.disabled = false;
+  intervalId = setInterval(changesColors, 1000);
+}
+
+function onStopButtonClick() {
+  StartButton.disabled = false;
+  StopButton.disabled = true;
+  clearInterval(intervalId);
+}
+
+function changesColors() {
+  bodyDocument.style.backgroundColor = getRandomHexColor();
+}
