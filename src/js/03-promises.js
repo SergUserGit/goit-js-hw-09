86eddbf9d16f963a7f46001e4f6fbc0a6e58d774
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-20 16:08:36 +0200

    part three begin
    part three begin

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+const formElement = document.querySelector('.form');
+formElement.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  console.log('Сабмит формы');
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
