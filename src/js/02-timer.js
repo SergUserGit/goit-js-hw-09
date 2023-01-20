65258fd9400e8636f318bdb06c026c495d3d1f28
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-20 12:48:36 +0200

    hours init
    hours init

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,7 @@ import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
 
+let intervalId = null;
 const dateTimePicker = document.querySelector('#datetime-picker');
 const buttonDataStart = document.querySelector('[data-start]');
 
@@ -27,10 +28,58 @@ flatpickr('#datetime-picker', options);
 buttonDataStart.addEventListener('click', onDataStartButtonClick);
 
 function onDataStartButtonClick() {
+  intervalId = setInterval(showTime, 1000);
+
+  //setInterval();
+  //  const TimePicker = new Date(dateTimePicker.value);
+  //  const currentDate = new Date(Date.now());
+  //  const dateDifference = TimePicker - currentDate;
+  //  console.log(dateDifference);
+  //  console.log(typeof dateDifference);
+  //alert('123');
+}
+
+function addLeadingZero(value) {
+  const strValue = String(value);
+  return strValue.padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function getDateDifference() {
   const TimePicker = new Date(dateTimePicker.value);
   const currentDate = new Date(Date.now());
-
   const dateDifference = TimePicker - currentDate;
-  console.log(dateDifference);
-  console.log(typeof dateDifference);
+  return dateDifference;
+}
+
+function showTime() {
+  const dateDifference = getDateDifference();
+  const objectTime = convertMs(dateDifference);
+  if (
+    objectTime.days === 0 &&
+    objectTime.hours === 0 &&
+    objectTime.minutes === 0 &&
+    objectTime.seconds === 0
+  ) {
+    clearInterval(intervalId);
+    return;
+  }
 }
